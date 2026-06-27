93e102b8fc68e5c2ac08634fc21803c94b278601
AleksandraSimeonova <209593041+AleksandraSimeonova@users.noreply.github.com>
2026-06-27 19:57:28 +0300

    Set up firebase

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import "./config/firebaseInit.js"
 import page from "./lib/page.js";
 
 import homeView from "./views/homeView.js";
---
+++ src/config/firebaseInit.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnTgcdpmuUluc7Grkum8FIFWjN1No2Idc",
+  authDomain: "dolni-vit-community-center.firebaseapp.com",
+  projectId: "dolni-vit-community-center",
+  storageBucket: "dolni-vit-community-center.firebasestorage.app",
+  messagingSenderId: "912589977063",
+  appId: "1:912589977063:web:a6a67328ac801deccdf942"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
